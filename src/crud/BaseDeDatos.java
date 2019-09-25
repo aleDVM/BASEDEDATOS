5b70070ea5e04239978c6e8327847c6dfaaf7683
aleDVM <pellahuenco@gmail.com>
2019-09-25 17:40:36 -0300

    comiteando segundo

--- src/crud/BaseDeDatos.java
+++ src/crud/BaseDeDatos.java
@@ -6,20 +6,23 @@ public class BaseDeDatos {
     Dato d = new Dato();
     ArrayList<Dato> datos = new ArrayList<>();
     
-    public ArrayList<Dato> Agregar(){       
+    public ArrayList<Dato> Agregar(String a, String b){       
         double r = Math.random()*40;
         d.setId(String.valueOf(r));
-        d.setData(d);
+        d.setData(a);
+        datos.add(d);
+        d.setData(b);
         datos.add(d);
         return datos;
     }
-    public void Eliminar(Dato d){
+    public void Eliminar(String d){
         datos.remove(d);
     }
-    public static Dato Listar(){
-        
+    public String Listar(){
+        return d.toString();
     }
     
     public static void Modificar (Dato d){
+        
     }
 }
--- src/crud/CRUD.java
+++ src/crud/CRUD.java
@@ -7,12 +7,14 @@ public class CRUD {
     public static void main(String[] args) {
         BaseDeDatos bd = new BaseDeDatos();
         Scanner scan = new Scanner(System.in);
-        System.out.println("Ingrese datos");
+        System.out.println("Ingrese nombre");
         String dato1 =  scan.next();
-        System.out.println("Ingrese datos");
+        System.out.println("Ingrese apellido");
         String dato2 =  scan.next();
-        bd.Agregar();
-        bd.Eliminar(null);
+        bd.Agregar(dato1, dato2);
+        System.out.println("Qué dato desea eliminar?");
+        String datoE = scan.next();
+        bd.Eliminar(datoE);
     }
     
 }
